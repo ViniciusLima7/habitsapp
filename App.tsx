bd954fc2033ee4438796bd7ec2d3f9717ea0bbb0
Vinicius Lima <marcos.vf@samsung.com>
2023-01-31 14:22:03 -0300

    Add Notificações

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import "./src/lib/dayjs";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Button } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
@@ -7,6 +7,7 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
+import * as Notifications from "expo-notifications";
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
 
@@ -18,16 +19,44 @@ export default function App() {
     Inter_800ExtraBold,
   });
 
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    }),
+  });
+
+  async function scheduleNotification() {
+    const trigger = new Date(Date.now());
+    trigger.setMinutes(trigger.getMinutes() + 1);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Olá, Vinicius!  🤩 ",
+        body: "Você praticou seus hábitos hoje?",
+      },
+      trigger,
+    });
+  }
+
+  async function getScheduleNotification() {
+    const schedules = await Notifications.getAllScheduledNotificationsAsync();
+    console.log(schedules);
+  }
+
   if (!fontsLoaded) {
     return <Loading />;
   }
   return (
     <>
+      {/* <Button title="Enviar Notificação" onPress={scheduleNotification} />
+      <Button title="Ver Agendadas" onPress={getScheduleNotification} /> */}
       <Routes />
       <StatusBar
         barStyle={"light-content"}
         backgroundColor="transparent"
-        translucent
+        // translucent
       />
     </>
   );
